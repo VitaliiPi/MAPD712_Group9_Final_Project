4f2fb806f0f32347d848124a26128ecca45b4881
VitaliiPi <44788449+VitaliiPi@users.noreply.github.com>
2020-11-26 22:43:01 +0200

    app refresh & pretifier

--- app/components/AddVitals.js
+++ app/components/AddVitals.js
@@ -1,26 +1,31 @@
-import React, { useEffect, useState,  } from 'react';
+import React, {useState} from 'react';
 import {
   ScrollView,
   StyleSheet,
-  BackHandler,
   View,
   Text,
+  Image,
   TextInput,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Toast from 'react-native-simple-toast';
 
-
-var url = "http://127.0.0.1:3009"
+var url = 'https://patientrecordsgroup.herokuapp.com';
 // screen for adding and editing vital
-export default function AddVitals({ navigation, route })  {
-  var vital = route.params.vital
+export default function AddVitals({navigation, route}) {
+  var vital = route.params.vital;
+
+  let errorMessage = '';
 
   //hooks for vitals info
   const [bloodPresure, setBloodPresure] = useState(vital.bloodPresure || '');
-  const [respiratoryRate, setRespiratoryRate] = useState(vital.respiratoryRate || '');
-  const [bloodOxigen, setBloodOxigen] = useState(vital.bloodOxigen || ''); hearthRate
+  const [respiratoryRate, setRespiratoryRate] = useState(
+    vital.respiratoryRate || '',
+  );
+  const [bloodOxigen, setBloodOxigen] = useState(vital.bloodOxigen || '');
+  hearthRate;
   const [hearthRate, setHearthRate] = useState(vital.hearthRate || '');
 
   // need for datetime picker
@@ -46,35 +51,111 @@ export default function AddVitals({ navigation, route })  {
     showMode('time');
   };
 
+  const createVitals = () => {
+    if (
+      bloodPresure.length === 0 &&
+      respiratoryRate.length === 0 &&
+      bloodOxigen.length === 0 &&
+      hearthRate.length === 0
+    ) {
+      errorMessage = 'no data';
+      console.log(errorMessage);
+      Toast.show(errorMessage, Toast.LONG);
+    } else {
+      let vital_id = vital._id !== undefined ? `/${vital._id}` : '';
+      let method = vital._id !== undefined ? 'PUT' : 'POST';
+      let new_vital = {
+        bloodPresure: bloodPresure,
+        respiratoryRate: respiratoryRate,
+        bloodOxigen: bloodOxigen,
+        hearthRate: hearthRate,
+        date: date,
+      };
+      fetch(url + `/patients/${route.params.patient._id}/records${vital_id}`, {
+        method: method,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(new_vital),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          errorMessage = 'all good';
+          navigation.navigate('ViewVitals', {
+            vital: json,
+            patient: route.params.patient,
+          });
+        })
+        .catch((error) => {
+          errorMessage = error.message;
+          console.log(errorMessage);
+          Toast.show(errorMessage, Toast.LONG);
+        });
+    }
+  };
+
+  const deleteVital = () => {
+    if (vital._id !== undefined){
+      fetch(
+        url + `/patients/${route.params.patient._id}/records/${vital._id}`,
+        {
+        method: 'DELETE',
+      })
+      .catch((error) => {
+        errorMessage = error.message;
+        Toast.show(errorMessage, Toast.LONG);
+      });
+    }
+    navigation.navigate('ViewPatient', {patient: route.params.patient})
+  }
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{alignSelf: 'flex-end'}}
+          onPress={() => deleteVital()}>
+          <Image
+            source={require('../assets/trash-bin.png')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
   return (
     <View style={styles.container}>
-      <ScrollView nestedScrollEnabled = {true}>
-      <Text style={[styles.text, { alignSelf: "center", fontSize: 28}]}>Measurements made</Text>
+      <ScrollView nestedScrollEnabled={true}>
+        <Text style={[styles.text, {alignSelf: 'center', fontSize: 28}]}>
+          Measurements made
+        </Text>
 
         <View style={styles.inLine}>
-        <TouchableOpacity style={{flex: 1}} onPress={showTimepicker}>
-            <Text style={[styles.text, { alignSelf: "center"}]} >
-              Time
-            </Text>
-            </TouchableOpacity>
+          <TouchableOpacity style={{flex: 1}} onPress={showTimepicker}>
+            <Text style={[styles.text, {alignSelf: 'center'}]}>Time</Text>
+          </TouchableOpacity>
           <TouchableOpacity style={{flex: 1}} onPress={showDatepicker}>
-            <Text style={[styles.text, { alignSelf: "center"}]} >
-              Date
-            </Text>
-            </TouchableOpacity>
+            <Text style={[styles.text, {alignSelf: 'center'}]}>Date</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.inLine}>
-        <TouchableOpacity style={{flex: 1}} onPress={showTimepicker}>
-        <Text style={[styles.text, {borderWidth:1, borderRadius: 5, alignSelf: "center", padding: 6}]} >
-              { date.getHours() + "-"+ date.getMinutes()}
+          <TouchableOpacity style={{flex: 1}} onPress={showTimepicker}>
+            <Text style={[styles.text, styles.textDateTime]}>
+              {date.getHours() + '-' + date.getMinutes()}
             </Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
           <TouchableOpacity style={{flex: 1}} onPress={showDatepicker}>
-          <Text style={[styles.text, {borderWidth:1, borderRadius: 5, alignSelf: "center", padding: 6}]} >
-              {date.getDate() + "-"+ date.getMonth() + "-"+ date.getFullYear()}
+            <Text style={[styles.text, styles.textDateTime]}>
+              {date.getDate() +
+                '-' +
+                date.getMonth() +
+                '-' +
+                date.getFullYear()}
             </Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
 
         {show && (
@@ -88,115 +169,100 @@ export default function AddVitals({ navigation, route })  {
           />
         )}
         <View>
+          <Text style={styles.text}>Blood Presure</Text>
+          <TextInput
+            style={styles.textinput}
+            keyboardType="number-pad"
+            value={bloodPresure}
+            onChangeText={(text) => setBloodPresure(text)}
+          />
 
-            <Text style={styles.text}>Blood Presure</Text>
-            <TextInput style={styles.textinput}
-              keyboardType='number-pad'
-              value = {bloodPresure}
-              onChangeText= {text => setBloodPresure(text)}
-            />
+          <Text style={styles.text}>Respiratory Rate</Text>
+          <TextInput
+            style={styles.textinput}
+            keyboardType="number-pad"
+            value={respiratoryRate}
+            onChangeText={(text) => setRespiratoryRate(text)}
+          />
 
-            <Text style={styles.text}>Respiratory Rate</Text>
-            <TextInput style={styles.textinput}
-              keyboardType='number-pad'
-              value = {respiratoryRate}
-              onChangeText= {text => setRespiratoryRate(text)}
-            />
+          <Text style={styles.text}>Blood Oxigen Level</Text>
+          <TextInput
+            style={styles.textinput}
+            keyboardType="number-pad"
+            value={bloodOxigen}
+            onChangeText={(text) => setBloodOxigen(text)}
+          />
 
-            <Text style={styles.text}>Blood Oxigen Level</Text>
-            <TextInput style={styles.textinput}
-              keyboardType='number-pad'
-              value = {bloodOxigen}
-              onChangeText= {text => setBloodOxigen(text)}
-            />
-
-            <Text style={styles.text}> Hearth Rate</Text>
-            <TextInput style={styles.textinput}
-              keyboardType='number-pad'
-              value = {hearthRate}
-              onChangeText= {text => setHearthRate(text)}
-            />
+          <Text style={styles.text}> Hearth Rate</Text>
+          <TextInput
+            style={styles.textinput}
+            keyboardType="number-pad"
+            value={hearthRate}
+            onChangeText={(text) => setHearthRate(text)}
+          />
         </View>
       </ScrollView>
 
       <View style={styles.bottom}>
         <TouchableOpacity
           style={[styles.button]}
-          onPress={() => 
-          {
-
-            if (bloodPresure.length==0 && respiratoryRate.length==0&&bloodOxigen.length==0&&hearthRate.length==0) {
-              Toast.show("no data", Toast.LONG)
-            }else {
-              let vital_id = (vital._id != undefined) ? `/${vital._id}` : ''
-              let method = (vital._id != undefined) ? `PUT` : 'POST'
-              let new_vital = {bloodPresure: bloodPresure,
-                            respiratoryRate: respiratoryRate,
-                            bloodOxigen: bloodOxigen,
-                            hearthRate: hearthRate,
-                            date: date
-                            }
-              fetch(url + `/patients/${route.params.patient._id}/records${vital_id}`, {
-                method: method,
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(new_vital)
-              })
-              .then((response) => response.json())
-              .then((json)=> {new_vital = json})
-              .then( () => {navigation.navigate( "ViewVitals", { vital: new_vital, patient: route.params.patient} ) })
-              .catch((error) => Toast.show(error.message, Toast.LONG))
-            }
-          }}
-        >
-            <Text style={styles.buttonText}>Save</Text>
+          onPress={() => createVitals()}>
+          <Text style={styles.buttonText}>Save</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create(
-  {
-    container:{
-      flex:1,
-      padding: 22,
-    },
-    button:{
-      backgroundColor: 'crimson',
-      borderRadius:25,
-      padding: 6,
-      margin: 5
-    },
-    text: {
-      fontFamily: "serif",
-      fontSize: 22,
-    },
-    inLine: {
-      flexDirection:'row',
-       marginBottom: 10,
-    },
-    bottom: {
-      justifyContent: 'flex-end'
-    },
-    buttonText:{
-      fontFamily: "serif",
-      color: "white",
-      alignSelf: 'center',
-      fontSize: 22,
-    },
-    textinput:{
-      fontFamily: "serif",
-      alignSelf: 'stretch',
-      height: 40,
-      fontSize: 20,
-      marginBottom: 15,
-      borderColor: '#0005',
-      borderRadius: 10 ,
-      borderWidth: 1,
-    },
-  }
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 22,
+  },
+  image: {
+    resizeMode: 'center',
+    paddingVertical: 5,
+    height: 50,
+    width: 50,
+    margin: 10,
+  },
+  button: {
+    backgroundColor: 'crimson',
+    borderRadius: 25,
+    padding: 6,
+    margin: 5,
+  },
+  text: {
+    fontFamily: 'serif',
+    fontSize: 22,
+  },
+  inLine: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  bottom: {
+    justifyContent: 'flex-end',
+  },
+  buttonText: {
+    fontFamily: 'serif',
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 22,
+  },
+  textinput: {
+    fontFamily: 'serif',
+    alignSelf: 'stretch',
+    height: 40,
+    fontSize: 20,
+    marginBottom: 15,
+    borderColor: '#0005',
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  textDateTime: {
+    borderWidth: 1,
+    borderRadius: 5,
+    alignSelf: 'center',
+    padding: 6,
+  },
+});
--- app/components/ViewPatients.js
+++ app/components/ViewPatients.js
@@ -1,180 +1,251 @@
-import React, { useEffect, useState, useLa } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   FlatList,
   StyleSheet,
   View,
   Text,
-  TextInput,
   Image,
+  Animated,
   TouchableOpacity,
 } from 'react-native';
 import Toast from 'react-native-simple-toast';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-var url = "http://127.0.0.1:3009"
-
-const compare = (a, b) => {
-  if (a.in_critical_condition && !b.in_critical_condition ||
-      a.in_critical_condition==b.in_critical_condition && a.name < b.name){
-      return -1
-  }
-  if (b.in_critical_condition && !a.in_critical_condition ||
-      a.in_critical_condition==b.in_critical_condition && a.name > b.name){
-    return 1  
-  }
-  return 0
-}
-
-export default function ViewPatients({ navigation, route })  {
+var url = 'https://patientrecordsgroup.herokuapp.com';
+let errorMessage = '';
+export default function ViewPatients({navigation, route}) {
   const [isLoading, setLoading] = useState(true);
   const [patientsList, setPatientsList] = useState([]);
-  console.log("view all patients", );
+  console.log('view all patients');
   // load list of patients
   const loadData = () => {
-    fetch(url + `/patients`)
+    console.log('loading all patients');
+    fetch(url + '/patients')
       .then((response) => response.json())
-      .then((json)=>setPatientsList(json))
+      .then((json) => {
+        setPatientsList([]);
+        setPatientsList(json.sort((a, b) => sorter(a, b)));
+      })
       .catch((error) => Toast.show(error.message, Toast.LONG))
       .finally(() => setLoading(false));
-    }
+  };
 
-    useEffect(() => { // TODO: change to oue domain
-      const unsubscribe = navigation.addListener('focus', () => {
-        loadData()
-      });
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      loadData();
+    });
   }, []);
 
-  React.useLayoutEffect(()=>{
+  React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight:()=>(
-        <TouchableOpacity style={styles.button}
-          onPress={() => navigation.navigate("AddPatient", { patient: '', user_id: route.params.user_id})}
-          >
-          <Text style={styles.buttonText}>{"  +  "}</Text>
+      headerRight: () => (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.navigate('AddPatient', {
+              patient: '',
+              user_id: route.params.user_id,
+            })
+          }>
+          <Text style={styles.buttonText}>{'  +  '}</Text>
         </TouchableOpacity>
-      )
-    })
-  
-  })
+      ),
+    });
+  });
 
-  
+  const deleteItem = (item, index) => {
+    console.log('delete ' + item._id);
+    
+    fetch(url + `/patients/${item._id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        row[index].close();
+        const arr = [...patientsList];
+        arr.splice(index, 1).sort((a, b) => sorter(a, b));
+        setPatientsList(arr);
+      })
+      .catch((error) => {
+        errorMessage = error.message;
+        Toast.show(errorMessage, Toast.LONG);
+      });
+  };
+
+  function sorter(a, b) {
+    let aname = a.name.toLowerCase();
+    let bname = b.name.toLowerCase();
+
+    if (a.in_critical_condition === b.in_critical_condition) {
+      return aname < bname ? -1 : aname > bname ? 1 : 0;
+    } else {
+      return a.in_critical_condition ? -1 : 1;
+    }
+  }
+
   return (
     <View style={styles.container}>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={[styles.firstColumn, styles.defaultFont]} />
+        <Text style={[styles.secondColumn, styles.defaultFont]}>Name</Text>
+        <Text style={[styles.thirdColumn, styles.defaultFont]}>State</Text>
+      </View>
 
-        <View style={{flexDirection:'row'}}>
-          <Text style={[styles.firstColumn, styles.defaultFont]}></Text>
-          <Text style={[styles.secondColumn, styles.defaultFont]}>Name</Text>
-          <Text style={[styles.thirdColumn, styles.defaultFont]}>State</Text>
-        </View>
-
-        {isLoading ? <ActivityIndicator/> : (
-          <FlatList
-          data={patientsList.sort(function(a,b){
-            aname = a.name.toLowerCase()
-            bname = b.name.toLowerCase()
-
-            if(a.in_critical_condition == b.in_critical_condition){
-                return (aname < bname) ? -1 : (aname > bname) ? 1 : 0;
-            }
-            else{
-                return a.in_critical_condition ? -1 : 1;
-            }
-          })}
-            renderItem={({ item }) => (
-              <ListItem item={item} navigation={navigation} user_id = {route.params.user_id}/>
-            )}
-          />
-        )}
-
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={patientsList}
+          renderItem={({item, index}) => {
+            return (
+              <ListItem
+                item={item}
+                index={index}
+                navigation={navigation}
+                user_id={route.params.user_id}
+                handleDelete={() => deleteItem(item, index)}
+              />
+            );
+          }}
+        />
+      )}
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create(
-  {
-    container:{
-      flex:1,
-      padding: 16,
-    },
-    firstColumn:{
-      flex: 2,
-      padding: 5,
-    },
-    secondColumn:{
-      flex: 10,
-      padding: 5,
-      fontSize: 18,
-    },
-    thirdColumn:{
-      flex: 2,
-      padding: 5,
-      fontSize: 18,
-      textAlign: 'center'},
-    button:{
-      backgroundColor: 'crimson',
-      borderRadius:25,
-      paddingVertical: 8,
-      paddingHorizontal: 6,
-      margin: 5,
-      alignSelf: 'flex-end',
-    },
-    defaultFont: {
-      fontFamily: "serif",
-    },
-    buttonText:{
-      color: "white",
-      alignSelf: 'center',
-      fontSize: 24,
-    },
-  }
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#EEEEEE',
+  },
+  firstColumn: {
+    flex: 2,
+    padding: 5,
+  },
+  secondColumn: {
+    flex: 10,
+    padding: 5,
+    fontSize: 18,
+  },
+  thirdColumn: {
+    flex: 2,
+    padding: 5,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'crimson',
+    borderRadius: 25,
+    paddingVertical: 8,
+    paddingHorizontal: 6,
+    margin: 5,
+    alignSelf: 'flex-end',
+  },
+  defaultFont: {
+    fontFamily: 'serif',
+  },
+  buttonText: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 24,
+  },
+  seperatorLine: {
+    height: 1,
+    backgroundColor: 'black',
+  },
+});
+
+let row: Array<any> = [];
 // Each item of the list is photo, full name, and status picture that redirect to patient's screen
-function ListItem(props){
+function ListItem(props) {
   let image, critical;
-  if (props.item.photo != undefined){
-    image = <Image source={{uri:props.item.photo}} style={listStyles.image} />
-  }else{
-    image = <Image source={require('../assets/person-icon.png')}
-                   style={listStyles.image} />
+  if (props.item.photo !== undefined) {
+    image = <Image source={{uri: props.item.photo}} style={listStyles.image} />;
+  } else {
+    image = (
+      <Image
+        source={require('../assets/person-icon.png')}
+        style={listStyles.image}
+      />
+    );
   }
-  critical = <Image source={ props.item.in_critical_condition ? require('../assets/critical_condition.png') : require('../assets/ok_state.png')} style={listStyles.image} />
+  critical = (
+    <Image
+      source={
+        props.item.in_critical_condition
+          ? require('../assets/critical_condition.png')
+          : require('../assets/ok_state.png')
+      }
+      style={listStyles.image}
+    />
+  );
 
+  const leftSwipe = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+    return (
+      <TouchableOpacity onPress={props.handleDelete} activeOpacity={0.6}>
+        <View style={listStyles.deleteBox}>
+          <Animated.Text style={{transform: [{scale: scale}]}}>
+            Delete
+          </Animated.Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <TouchableOpacity
-      key={props.item.id}
-      style={listStyles.container}
-      onPress={() =>
-        props.navigation.navigate('ViewPatient', { patient: props.item, user_id: props.user_id })
-      }>
-
-      {image}
-      <Text style={listStyles.text}>{props.item.name}</Text>
-      {critical}
-
-    </TouchableOpacity>);
+    <Swipeable renderLeftActions={leftSwipe}
+      ref={(ref) => (row[props.index] = ref)}>
+      <TouchableOpacity
+        key={props.item.id}
+        style={listStyles.container}
+        onPress={() =>
+          props.navigation.navigate('ViewPatient', {
+            patient: props.item,
+            user_id: props.user_id,
+          })
+        }>
+        {image}
+        <Text style={listStyles.text}>{props.item.name}</Text>
+        {critical}
+      </TouchableOpacity>
+    </Swipeable>
+  );
 }
-const listStyles = StyleSheet.create(
-  {
-    container:{
-      flex: 1,
-      flexDirection: 'row',
-      borderBottomWidth: 1,
-    },
-    image:{
-      flex:2,
-      alignSelf: 'center',
-      resizeMode: "center",
-      paddingVertical: 5,
-      height: 60,
-    },
-    text:{
-      fontFamily: "serif",
-      alignSelf: 'center',
-      marginBottom:10,
-      padding:5,
-      fontSize: 20,
-      flex:10,
-    },
-  }
-);
+const listStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    height: 80,
+    backgroundColor: '#EEEEEE',
+    paddingBottom: 2,
+  },
+  image: {
+    flex: 2,
+    alignSelf: 'center',
+    resizeMode: 'center',
+    paddingVertical: 5,
+    height: 60,
+  },
+  text: {
+    fontFamily: 'serif',
+    alignSelf: 'center',
+    marginBottom: 10,
+    padding: 5,
+    fontSize: 20,
+    flex: 10,
+  },
+  deleteBox: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 80,
+    borderRadius: 5,
+  },
+});
